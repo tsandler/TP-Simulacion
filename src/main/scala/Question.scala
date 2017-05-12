a3f02bfe5b18a4f41dfd76dff1478c9542c7f1c4
Tobias Sandler <tsandler@adistec.com>
2017-05-12 12:54:15 -0300

    mejorada la clase Question

--- src/main/scala/Question.scala
+++ src/main/scala/Question.scala
@@ -1,30 +1,24 @@
 
 class Question {
 
-  var function: (Long, Long) => Boolean =_
-  var firstValue: Long =_
-  var secondValue: Long =_
+  var result: Boolean =_
 
-  def this(_function: (Long, Long) => Boolean, _firstValue: Long, _secondValue: Long){
+  def this(function: (Long, Long) => Boolean, firstValue: Long, secondValue: Long){
     this()
-    this.function = _function
-    this.firstValue = _firstValue
-    this.secondValue = _secondValue
+    this.result = function(firstValue, secondValue)
   }
 
   def accept(action: () => Unit) ={
-    if (acceptValue){
+    if (result){
       action.apply()
     }
     this
   }
 
   def reject(action: () => Unit) ={
-    if (!acceptValue){
+    if (!result){
       action.apply()
     }
     this
   }
-
-  def acceptValue = function(firstValue, secondValue)
 }
